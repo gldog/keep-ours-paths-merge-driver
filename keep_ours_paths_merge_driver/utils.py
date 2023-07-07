971cfcd62ab46888c89cc09b9b805085fc08dda0
Johannes Heger <gldog@gmx.de>
2023-07-07 22:01:44 +0200

    Fix warning 'shadows name from outer scope'

--- keep_ours_paths_merge_driver/utils.py
+++ keep_ours_paths_merge_driver/utils.py
@@ -3,27 +3,26 @@ import logging
 logger = logging.getLogger()
 
 
-def replace_nth(s, search_token, replace_token, position):
-    index_of_search_token = 0
+def replace_nth(s, search_token, replacement_token, position):
     start_search_at = 0
     for i in range(position):
         index_of_search_token = s.find(search_token, start_search_at)
         if index_of_search_token == -1:
             return ''
         if i == position - 1:
-            return s[0:index_of_search_token] + replace_token + s[index_of_search_token + len(search_token):]
+            return s[0:index_of_search_token] + replacement_token + s[index_of_search_token + len(search_token):]
         start_search_at = index_of_search_token + 1
 
     return ''
 
 
-def replace_token(s, search_token, replace_token, compare_to_reference):
-    logger.debug(f"replace_token(); search_token: {search_token}; replace_token: {replace_token}")
+def replace_token(s, search_token, replacement_token, compare_to_reference):
+    logger.debug(f"replace_token(); search_token: {search_token}; replace_token: {replacement_token}")
     str_replaced_at_n = ''
     n = 1
     while True:
         # replace_nth() returns an empty string in case there is no searchToken at n.
-        str_replaced_at_n = replace_nth(s, search_token, replace_token, n)
+        str_replaced_at_n = replace_nth(s, search_token, replacement_token, n)
         if not str_replaced_at_n:
             break
 
